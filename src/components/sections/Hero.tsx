d976759e11662fc018e060478e04650d4ee38184
nicoalvarez03 <nicoo.alvarez03@gmail.com>
2025-06-27 14:11:29 +0200

    feat: rediseño de botones e implementacion de botones github y descarga de CV

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,8 +1,10 @@
 import foto_nicolas_carnet from "../../assets/images/foto_nicolas_carnet.webp";
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 import { ArrowDown } from "../common/arrows/ArrowDown";
 import { ButtonSecondary } from "../common/buttons/ButtonSecondary";
 import { Reveal } from "../common/Reveal";
+import pdfEs from '../../PDFDownload/Currículum_Nicolas_Alvarez_Espanol.pdf';
+import { MdOutlineFileDownload } from "react-icons/md";
 
 export default function Hero() {
   return (
@@ -10,59 +12,76 @@ export default function Hero() {
       <div className="flex items-center justify-center">
         <div className="flex flex-col gap-2">
           <div className="flex items-center gap-10">
-          <div className="flex justify-center">
-            <Reveal>
-              <div>
-                <img
-                  className="w-50 h-50 object-cover rounded-full"
-                  src={foto_nicolas_carnet}
-                  alt="foto carnet"
-                />
-              </div>
-            </Reveal>
-          </div>
+            <div className="flex justify-center">
+              <Reveal>
+                <div>
+                  <img
+                    className="w-50 h-50 object-cover rounded-full"
+                    src={foto_nicolas_carnet}
+                    alt="foto carnet"
+                  />
+                </div>
+              </Reveal>
+            </div>
             <div>
-                <Reveal>
+              <Reveal>
                 <h1 className="mb-4 text-6xl tracking-wide font-bold">
-                    Hola, soy Nicolás Álvarez
-                    <strong className="text-6xl text-[#9191ee]">.</strong>
+                  Hola, soy Nicolás Álvarez
+                  <strong className="text-6xl text-[#9191ee]">.</strong>
                 </h1>
-                </Reveal>
-                <Reveal>
+              </Reveal>
+              <Reveal>
                 <p className="text-xl tracking-widest font-semibold">
-                    Y soy{" "}
-                    <strong className="text-4xl text-[#9191ee] font-bold">
+                  Y soy{" "}
+                  <strong className="text-4xl text-[#9191ee] font-bold">
                     Desarrollador Web Full Stack
-                    </strong>
+                  </strong>
                 </p>
-                </Reveal>
+              </Reveal>
             </div>
           </div>
           <div className="flex justify-end">
-          <Reveal>
-            <div className="w-190">
-                <p>Gran motivación para seguir perfeccionando habilidades y crecer profesionalmente. Confío plenamente en mis capacidades como profesional. Através de mi experiencia en diferentes organizaciones he desarrollado amplías habilidades analíticas, de comunicación y excelente capacidad para trabajar en equipo.</p>
-            </div>
-          </Reveal>
-          </div>
-          
-          <div className="flex justify-center items-center mt-20">
             <Reveal>
-              <div className="w-50 h-12 flex justify-center items-center">
-                <ButtonSecondary onClick={() => {
-                  const section = document.getElementById("inicio");
-                  if (section) {
-                    section.scrollIntoView({ behavior: "smooth" });
-                  }
-                }} 
-                className="w-40 h-10" >
-                  Contactame
-                </ButtonSecondary>
+              <div className="w-190">
+                <p>
+                  Gran motivación para seguir perfeccionando habilidades y
+                  crecer profesionalmente. Confío plenamente en mis capacidades
+                  como profesional. Através de mi experiencia en diferentes
+                  organizaciones he desarrollado amplías habilidades analíticas,
+                  de comunicación y excelente capacidad para trabajar en equipo.
+                </p>
               </div>
             </Reveal>
           </div>
+
+          <div className="w-full flex justify-center mt-20">
+            <div className="w-full flex items-center justify-center gap-2">
+              <Reveal>
+                <ButtonSecondary
+                  onClick={() => {
+                    const section = document.getElementById("inicio");
+                    if (section) {
+                      section.scrollIntoView({ behavior: "smooth" });
+                    }
+                  }}
+                  className="w-40 h-10 m-5"
+                >
+                  Contactame
+                </ButtonSecondary>
+              </Reveal>
+
+              <Reveal>
+                <ButtonSecondary
+                  className="w-50 h-10 m-5 gap-2"
+                >
+                  <a href={pdfEs} target='_blank' rel='noopener noreferrer' download="Currículum_Nicolas_Alvarez_Espanol.pdf" className='text-[12px]'>Descargar CV Español</a>
+                  <MdOutlineFileDownload/>
+                </ButtonSecondary>
+              </Reveal>
+            </div>
+          </div>
           <div className="flex justify-center mt-30">
-            <motion.a 
+            <motion.a
               onClick={() => {
                 const section = document.getElementById("proyectos");
                 if (section) {
